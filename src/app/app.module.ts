186c4aee69c4cc4d4832ec6a340dc42c548a92e4
Ivy Dekker <ivy_dk@hotmail.com>
2024-10-11 10:33:51 +0200

    dark mode

---
+++ src/app/app.module.ts
@@ -0,0 +1,8 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+imports: [
+  BrowserModule,
+  NgbModule,
+  AppRoutingModule,
+],
